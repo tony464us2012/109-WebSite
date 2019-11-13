e4faa153808ed8c8886c085314e44939ca4cf5a6
Anthony <tony464us2011@yahoo.com>
2019-11-13 01:52:45 -0500

    made it better

---
+++ client/build/precache-manifest.0b2b1ed5a6931f720009ca50262aa863.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf66783439a32162ad30d54942f0d06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cac71e3744d99c9447c",
+    "url": "/static/css/main.f00e53fc.chunk.css"
+  },
+  {
+    "revision": "3e3acb417128367ff2d0",
+    "url": "/static/js/2.f51c4a9a.chunk.js"
+  },
+  {
+    "revision": "4cac71e3744d99c9447c",
+    "url": "/static/js/main.ff0c7502.chunk.js"
+  },
+  {
+    "revision": "e62c1e9c25e7bddf9424",
+    "url": "/static/js/runtime-main.c90da625.js"
+  },
+  {
+    "revision": "95f15cb31020aa3f51b74760c102d980",
+    "url": "/static/media/logo-01.95f15cb3.png"
+  }
+]);
